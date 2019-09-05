723fc2da062aa487693bbda1b052bb6fd6244f5c
ThomasVindelev <thom891b@stud.kea.dk>
2019-09-05 10:35:17 +0200

    Adding POJOs

---
+++ src/Course.java
@@ -0,0 +1,57 @@
+import java.util.List;
+
+public class Course {
+
+    private int id;
+    private String courseName;
+    private List<Student> students;
+    private List<Teacher> teachers;
+    private List<Exam> exams;
+
+    public Course(String courseName, List<Student> students, List<Teacher> teachers, List<Exam> exams) {
+        this.courseName = courseName;
+        this.students = students;
+        this.teachers = teachers;
+        this.exams = exams;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public List<Teacher> getTeachers() {
+        return teachers;
+    }
+
+    public void setTeachers(List<Teacher> teachers) {
+        this.teachers = teachers;
+    }
+
+    public List<Exam> getExams() {
+        return exams;
+    }
+
+    public void setExams(List<Exam> exams) {
+        this.exams = exams;
+    }
+}
